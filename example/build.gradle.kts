e5c4a6c05031f4e55022d7976d978e02b8e857d1
Michael obi <michael@paystack.com>
2020-09-10 11:26:03 +0100

    Migrate all gradle scripts to kts

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,21 +1,20 @@
 plugins {
-    id("com.android.library")
+    id("com.android.application")
     kotlin("android")
-    kotlin("kapt")
 }
 
 android {
     compileSdkVersion(30)
-    buildToolsVersion = "29.0.3"
+    buildToolsVersion("29.0.3")
 
     defaultConfig {
+        applicationId("com.paystack.checkout.example")
         minSdkVersion(21)
         targetSdkVersion(30)
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -27,36 +26,21 @@ android {
             )
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-
     kotlinOptions {
         jvmTarget = "1.8"
-        freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
-    }
-
-    lintOptions {
-        enable("Interoperability")
     }
 }
 
 dependencies {
+
     implementation(Libs.Kotlin.stdlib)
     implementation(Libs.AndroidX.coreKtx)
     implementation(Libs.AndroidX.appcompat)
-
-    implementation(Libs.Retrofit.retrofit)
-    implementation(Libs.Retrofit.moshiConverter)
-    implementation(Libs.OkHttp.okhttp)
-    implementation(Libs.OkHttp.loggingInterceptor)
-
-    implementation(Libs.kotlinResult)
-
-    implementation(Libs.Moshi.moshi)
-    kapt(Libs.Moshi.moshiCodegen)
+    implementation(project(":checkout"))
 
     testImplementation(Libs.junit)
     testImplementation(Libs.mockitoKotlin)
