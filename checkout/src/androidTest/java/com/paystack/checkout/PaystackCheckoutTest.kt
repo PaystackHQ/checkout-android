16478c5c4fe711a2f3a61b45f2f7ada8e7e55b0e
Michael obi <michael@paystack.com>
2020-09-11 17:59:19 +0100

    Define developer facing APIs

--- checkout/src/androidTest/java/com/paystack/checkout/PaystackCheckoutTest.kt
+++ checkout/src/androidTest/java/com/paystack/checkout/PaystackCheckoutTest.kt
@@ -1,26 +1,103 @@
 package com.paystack.checkout
 
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.result.ActivityResultRegistry
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.core.app.ActivityOptionsCompat
 import androidx.test.core.app.ActivityScenario
+import com.nhaarman.mockitokotlin2.verify
+import com.paystack.checkout.model.ChargeResult
+import com.paystack.checkout.model.Transaction
 import com.paystack.checkout.ui.CheckoutActivity
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
 
 class PaystackCheckoutTest {
 
+    @Mock
+    lateinit var resultListener: CheckoutResultListener
+
     @Before
     fun setUp() {
-    }
-
-    @After
-    fun tearDown() {
+        MockitoAnnotations.initMocks(this)
     }
 
     @Test
-    fun charge() {
-        ActivityScenario.launch(CheckoutActivity::class.java).use { activity ->
-            assert(activity is AppCompatActivity)
+    fun charge_whenChargeIsSuccessful_invokesResultListenerOnSuccess() {
+        ActivityScenario.launch(CheckoutActivity::class.java).use { scenario ->
+            scenario.onActivity { activity ->
+                val chargeResult = ChargeResult.Success(SUCCESSFUL_TRANSACTION)
+                val testRegistry = getTestResultRegistry(chargeResult)
+                val paystackCheckout =
+                    PaystackCheckout(activity, "pk_test_1234568695", testRegistry)
+                paystackCheckout.charge(TEST_EMAIL, 10000, "NGN", resultListener)
+
+                verify(resultListener).onSuccess(SUCCESSFUL_TRANSACTION)
+            }
         }
+    }
+
+    @Test
+    fun charge_whenChargeFails_invokesResultListenerOnError() {
+        ActivityScenario.launch(CheckoutActivity::class.java).use { scenario ->
+            scenario.onActivity { activity ->
+                val chargeResult = ChargeResult.Error(FAILED_TRANSACTION)
+                val testRegistry = getTestResultRegistry(chargeResult)
+                val paystackCheckout =
+                    PaystackCheckout(activity, "pk_test_1234568695", testRegistry)
+                paystackCheckout.charge(TEST_EMAIL, 10000, "NGN", resultListener)
+
+                verify(resultListener).onError(FAILED_TRANSACTION)
+            }
+        }
+    }
+    @Test
+    fun charge_whenCheckoutIsCancelled_invokesResultListenerOnCancelled() {
+        ActivityScenario.launch(CheckoutActivity::class.java).use { scenario ->
+            scenario.onActivity { activity ->
+                val testRegistry = getTestResultRegistry(ChargeResult.Cancelled)
+                val paystackCheckout =
+                    PaystackCheckout(activity, "pk_test_1234568695", testRegistry)
+                paystackCheckout.charge(TEST_EMAIL, 10000, "NGN", resultListener)
+
+                verify(resultListener).onCancelled()
+            }
+        }
+    }
+
+    private fun getTestResultRegistry(chargeResult: ChargeResult): ActivityResultRegistry {
+        return object : ActivityResultRegistry() {
+            override fun <I, O> onLaunch(
+                requestCode: Int,
+                contract: ActivityResultContract<I, O>,
+                input: I,
+                options: ActivityOptionsCompat?
+            ) {
+                dispatchResult(requestCode, chargeResult)
+            }
+        }
+    }
+
+    companion object {
+        const val TEST_EMAIL = "michael@paystack.com"
+
+        val FAILED_TRANSACTION = Transaction(
+            id = 1000,
+            accessCode = "failed_access",
+            amount = 10000,
+            currency = "NGN",
+            email = TEST_EMAIL,
+            transactionStatus = "failed"
+        )
+        val SUCCESSFUL_TRANSACTION = Transaction(
+            id = 1001,
+            accessCode = "successful_access",
+            amount = 10000,
+            currency = "NGN",
+            email = TEST_EMAIL,
+            transactionStatus = "successful"
+        )
     }
 }
--- checkout/src/main/java/com/paystack/checkout/PaystackCheckout.kt
+++ checkout/src/main/java/com/paystack/checkout/PaystackCheckout.kt
@@ -1,16 +1,36 @@
 package com.paystack.checkout
 
-import android.widget.Toast
+import androidx.activity.result.ActivityResultRegistry
 import androidx.appcompat.app.AppCompatActivity
 import com.paystack.checkout.model.ChargeParams
+import com.paystack.checkout.model.ChargeResult
 import com.paystack.checkout.ui.PayWithCheckout
 
-class PaystackCheckout(private val activity: AppCompatActivity, private val publicKey: String) {
+class PaystackCheckout {
+    private val activity: AppCompatActivity
+    private val activityResultRegistry: ActivityResultRegistry
+    private val publicKey: String
 
-    fun charge(email: String, amount: Long, currency: String) {
+    constructor(activity: AppCompatActivity, publicKey: String) {
+        this.activity = activity
+        this.activityResultRegistry = activity.activityResultRegistry
+        this.publicKey = publicKey
+    }
+
+    constructor(activity: AppCompatActivity, publicKey: String, activityResultRegistry: ActivityResultRegistry) {
+        this.activity = activity
+        this.activityResultRegistry = activityResultRegistry
+        this.publicKey = publicKey
+    }
+
+    fun charge(email: String, amount: Long, currency: String, resultListener: CheckoutResultListener) {
         val params = ChargeParams(publicKey, email, amount, currency)
-        activity.registerForActivityResult(PayWithCheckout()) {
-            Toast.makeText(activity, it.toString(), Toast.LENGTH_SHORT).show()
+        activity.registerForActivityResult(PayWithCheckout(), activityResultRegistry) { chargeResult ->
+            when (chargeResult) {
+                is ChargeResult.Success -> resultListener.onSuccess(chargeResult.transaction)
+                is ChargeResult.Error -> resultListener.onError(chargeResult.transaction)
+                ChargeResult.Cancelled -> resultListener.onCancelled()
+            }
         }.launch(params)
     }
 }
